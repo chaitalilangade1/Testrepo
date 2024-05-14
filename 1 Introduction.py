a8d05c263275b9be1ba51dfc9a2fa755f0484f3d
chaitali <chaitalilangade1@gmail.com>
2024-05-14 16:55:17 -0400

    add new file

---
+++ 1 Introduction.py
@@ -0,0 +1,24 @@
+#while loop:
+'''
+-while loop executes set of stmts for repeated number of times or for multiple times.
+-All the stmts within the while loop should follow the indentation
+
+#systax for defining while loop
+initialization
+while(condition):
+    stmt1
+    stmt2
+    stmt3
+    stmt4
+    increment/decrement
+
+-the stmts within the while loop will be executed until the condition become False once the condition becomes False it comes out of while loop and execute the other stmts.
+
+'''
+
+#program to print number from 1 to 10.
+x=1                  #Initilization
+while(x<=10):        #condition
+    print(x)
+    x=x+1            #increment
+#print("end")
